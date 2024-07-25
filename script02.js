6528711949805fa9c71e316adf657bf586d921c9
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-07-25 17:12:50 -0300

    anotando o obgetivo do codigo

---
+++ script02.js
@@ -0,0 +1,6 @@
+/*Armazene essa informação em uma variável chamada intensidade.
+Gerar os aplausos: Utilize um laço de repetição for para exibir no console a 
+quantidade de palmas "👏" correspondente a intensidade escolhida.*/
+
+let intensidade = 0;
+let palmas = "20";
