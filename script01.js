36cb88e7191e6196c3b3316a043a70a557f965e5
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-07-25 17:07:21 -0300

    organizando anotações atividade 01

--- script01.js
+++ script01.js
@@ -1,5 +1,9 @@
 //verificar se a idade do aluno é maior do que 7 anos.
 
+/*-Infantil: para alunos de 8 a 12 anos.
+-Adolescentes: para alunos de 13 a 17 anos. 
+-Adultos: para alunos maiores de 18 anos*/
+
 let idadeAluno = 13;
 
 if (idadeAluno > 7 && idadeAluno < 13) {
@@ -12,7 +16,5 @@ if (idadeAluno > 7 && idadeAluno < 13) {
     console.log("aluno não pode se matricular");
 }
 
-/*-Infantil: para alunos de 8 a 12 anos.
--Adolescentes: para alunos de 13 a 17 anos. 
--Adultos: para alunos maiores de 18 anos*/
+
 
